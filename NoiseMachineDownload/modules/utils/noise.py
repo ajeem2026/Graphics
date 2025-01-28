785bac2ce06a2be26fe01143cfb0514f3935930c
Abid Jeem <ajeem@mail.wlu.edu>
2025-01-27 20:28:27 -0500

    Completed all requirements of assignment.

--- NoiseMachineDownload/modules/utils/noise.py
+++ NoiseMachineDownload/modules/utils/noise.py
@@ -7,19 +7,20 @@ import numpy as np
 from .vector import smerp, lerp
 from .definitions import COLORS
 
+
 class NoiseMachine:
     def __init__(self,
                  noctaves=5,
                  octaveDilation=2,
-                 minimum = 0,
-                 maximum = 1,
+                 minimum=0,
+                 maximum=1,
                  nvalues=256,
                  seed=1234):
         self.noctaves = noctaves
         self.octaveDilation = octaveDilation
         self.nvalues = nvalues
-        self.values = np.linspace(minimum,maximum,nvalues)
-        self.permutations = np.arange(0,nvalues,1)
+        self.values = np.linspace(minimum, maximum, nvalues)
+        self.permutations = np.arange(0, nvalues, 1)
         np.random.seed(seed)
         np.random.shuffle(self.values)
         np.random.shuffle(self.permutations)
@@ -38,7 +39,7 @@ class NoiseMachine:
         s = 0.0
         for i in range(self.noctaves):
             s += self.smerpNoise(x*self.octaveDilation**i)/2**i
-        return s*0.5 
+        return s*0.5
 
     # Two-dimensional noise
     def intNoise2d(self, i, j):
@@ -48,19 +49,19 @@ class NoiseMachine:
         b = self.permutations[(i + a) % self.nvalues]
         return self.values[b % self.nvalues]
 
-    def smerpNoise2d(self, x,y):
+    def smerpNoise2d(self, x, y):
         """Smoothly interpolate given two dimensional points."""
         i = int(np.floor(x))
         j = int(np.floor(y))
         xFrac = x-i
         yFrac = y-j
-        
+
         # randoms at four corners:
-        n00 = self.intNoise2d(i,j)
-        n10 = self.intNoise2d(i+1,j)
-        n01 = self.intNoise2d(i,j+1)
-        n11 = self.intNoise2d(i+1,j+1)
-        
+        n00 = self.intNoise2d(i, j)
+        n10 = self.intNoise2d(i+1, j)
+        n01 = self.intNoise2d(i, j+1)
+        n11 = self.intNoise2d(i+1, j+1)
+
         # smerp along x
         nx0 = smerp(n00, n10, xFrac)
         nx1 = smerp(n01, n11, xFrac)
@@ -74,7 +75,6 @@ class NoiseMachine:
             s += self.smerpNoise2d(x*self.octaveDilation**i,
                                    y*self.octaveDilation**i)/2**i
         return s*0.5
- 
 
     def noise2dTiled(self, x, y, xMod, yMod):
         """Cumulative noise at x and y using smerp, tilable."""
@@ -85,51 +85,128 @@ class NoiseMachine:
                                         xMod*self.octaveDilation**i,
                                         yMod*self.octaveDilation**i)/2**i
         return s*0.5
-    
+
     def smerpNoise2dTiled(self, x, y, xMod, yMod):
         """Smoothly interpolate given two dimensional points, tilable."""
         i = int(np.floor(x))
         j = int(np.floor(y))
         xFrac = x-i
         yFrac = y-j
-        
+
         # randoms at four corners:
-        n00 = self.intNoise2d(    i % xMod,     j % yMod)
+        n00 = self.intNoise2d(i % xMod,     j % yMod)
         n10 = self.intNoise2d((i+1) % xMod,     j % yMod)
-        n01 = self.intNoise2d(    i % xMod, (j+1) % yMod)
+        n01 = self.intNoise2d(i % xMod, (j+1) % yMod)
         n11 = self.intNoise2d((i+1) % xMod, (j+1) % yMod)
-        
+
         # smerp along x
         nx0 = smerp(n00, n10, xFrac)
         nx1 = smerp(n01, n11, xFrac)
         # smerp along y
         return smerp(nx0, nx1, yFrac)
 
+
 class NoisePatterns(object):
     _instance = None
-    
+
     @classmethod
     def getInstance(cls):
         if cls._instance == None:
             cls._instance = NoisePatterns()
         return cls._instance
-    
+
     def __init__(self):
         self.noiseId = 0
         self.scale = 50
         self.nms = [NoiseMachine(seed=i) for i in range(5)]
-    
-    def next(self):        
+
+    def next(self):
         self.noiseId += 1
         self.noiseId %= len(self.nms)
-    
-    def previous(self):        
+
+    def previous(self):
         self.noiseId -= 1
         self.noiseId %= len(self.nms)
-    
+
     def clouds(self, x, y,
                c1=COLORS["blue"], c2=COLORS["white"]):
-        noise = self.nms[self.noiseId].noise2d(x,y)
+        noise = self.nms[self.noiseId].noise2d(x, y)
         return lerp(c1, c2, noise)
 
-                 
+    # TODO: Create a cloudsTiled() function
+
+    def cloudsTiled(self, x, y, xMod, yMod, c1=COLORS["blue"], c2=COLORS["white"]):
+        """Cumulative noise at x and y using smerp, tilable."""
+        noise = self.nms[self.noiseId].noise2dTiled(x,y, xMod, yMod)
+        return lerp(c1, c2, noise)
+    
+    # TODO: Create a marble() function
+    
+    def marble(self, x, y, c1=COLORS["marble1"], c2=COLORS["marble2"], noiseStrength=0.2):
+        """Cumulative noise at x and y using smerp, tilable."""
+        
+        #At a given x and y, obtain a noise2d() value
+        noise = self.nms[self.noiseId].noise2d(x,y)
+        
+        #Then calculate the sine of x + y + noise * noiseStrength * scale to obtain a value between -1 and +1
+        
+        sine= np.sin(x + y + noise * noiseStrength * self.scale)
+        
+        #Adjust the result to be a value between 0 and 1 and use the value to linearly interpolate between c1 and c2
+        
+        normalised= (sine+1)/2
+        
+        return lerp(c1, c2, normalised)
+    
+    # TODO: Create a wood() function
+    
+    def wood(self, x, y, c1=COLORS["wood1"], c2=COLORS["wood2"], noiseStrength=0.2):
+        """Cumulative noise at x and y using smerp, tilable."""
+        #At a given x and y, obtain a noise2d() value
+        noise = self.nms[self.noiseId].noise2d(x,y)
+        
+        #Calculate the radius value as the square-root of x squared plus y squared, then multiply by 10
+        
+        radius= np.sqrt(x**2 + y**2)*10
+        
+        #Then calculate the sine of radius + noise * noiseStrength * scale to obtain a value between -1 and +1
+        
+        sine= np.sin(radius+ noise * noiseStrength * self.scale)
+        
+        #Adjust the result to be a value between 0 and 1 and use the value to linearly interpolate between c1 and c2
+        
+        normalised= (sine+1)/2
+        
+        return lerp(c1, c2, normalised)
+
+
+    # TODO: Create a fire() function
+    
+    def fire(self, x, y, c1=COLORS["red"], c2=COLORS["yellow"], noiseStrength=0.8):
+        """Cumulative noise at x and y using smerp, tilable."""
+        y = y / 2
+       
+        #To calculate the fire shape, get the color by obtaining a noise2d() value at x*2 and y*2 and using the noise to interpolate between c1 and c2
+        noise = self.nms[self.noiseId].noise2d(x*2,y*2)
+        
+        initial_color= lerp(c1, c2, noise)
+        
+        #Calculate a radius about a midpoint by np.sqrt((x-xMiddle)**2 + (y-yMiddle)**2)/4. Set the midpoint to 4, 3.
+        xMiddle= 4
+        yMiddle=3
+        radius= np.sqrt((x-xMiddle)**2 + (y-yMiddle)**2)/4
+        
+        #======== For the wiggles ======================
+        
+        #For the wiggles, obtain a new noise2d() at x + sine(y * 2) * 0.5, y
+        newNoise= self.nms[self.noiseId].noise2d(x+np.sin(y*2)*0.5,y)
+        
+        #Increase the radius value from before by (noise - 0.5) * noiseStrength.
+        
+        radius+= (newNoise - 0.5) * noiseStrength
+        
+        #Then calculate a color multiplier s as 1.0 - smerp(0.1, 1.0, radius)
+        
+        s= 1.0 - smerp(0.1, 1.0, radius)
+        
+        return initial_color*s
--- NoiseMachineDownload/renderDemo.py
+++ NoiseMachineDownload/renderDemo.py
@@ -7,6 +7,9 @@ import random
 
 
 class RandomRenderer(ProgressiveRenderer):
+
+    # Shows how renderer is restarted
+    # This is mainly for artifacts that don't look right in lower res
     def __init__(self, width=640, height=480,
                  showTime=True,
                  show=ShowTypes.PerColumn,
@@ -15,25 +18,194 @@ class RandomRenderer(ProgressiveRenderer):
         """An unnecessary override but provided to show how
         to override the __init__ in future inheritance classes."""
         super().__init__(width, height,
-                 showTime,
-                 show,
-                 minimumPixel,
-                 startPixelSize)
-        
+                         showTime,
+                         show,
+                         minimumPixel,
+                         startPixelSize)
+
     def getColor(self, x, y):
         """Gives a random color per pixel."""
         return np.array((random.random(),
                          random.random(),
                          random.random()))
-    
+
     def handleOtherInput(self, event):
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             type(self).restart()
 
-    
+# TODO: Create a RainbowRenderer class here
+# What is color based on? x and y coord as a % of total width and height
+# Horizontal %: Red component and Vertical %: Green component
+# 100% - Horizontal %: Blue component
+
+
+class RainbowRenderer(ProgressiveRenderer):
+
+    # Shows how renderer is restarted
+    # This is mainly for artifacts that don't look right in lower res
+    def __init__(self, width=640, height=480,
+                 showTime=True,
+                 show=ShowTypes.PerColumn,
+                 minimumPixel=0,
+                 startPixelSize=256):
+        """An unnecessary override but provided to show how
+        to override the __init__ in future inheritance classes."""
+        super().__init__(width, height,
+                         showTime,
+                         show,
+                         minimumPixel,
+                         startPixelSize)
+
+        # Initialising each component
+        self.red_mode = 0
+        self.green_mode = 1
+        self.blue_mode = 2
+
+    def getColor(self, x, y):
+        """Component Calculation"""
+
+        width = self.width
+        height = self.height
+
+        # Calculations for the red component
+
+        # For horizontal
+        if self.red_mode == 0:
+            red = x / width
+
+        # For vertical
+
+        elif self.red_mode == 1:
+            red = y / height
+
+        # For horizontal complement
+        elif self.red_mode == 2:
+            red = 1 - (x / width)
+        # For vertical complement
+        elif self.red_mode == 3:
+            red = 1 - (y / height)
+        else:
+            red = 0
+
+        # Calculations for the green component
+        if self.green_mode == 0:
+            green = x / width
+        elif self.green_mode == 1:
+            green = y / height
+        elif self.green_mode == 2:
+            green = 1 - (x / width)
+        elif self.green_mode == 3:
+            green = 1 - (y / height)
+        else:
+            green = 0
+
+       # Calculations for the blue component
+        if self.blue_mode == 0:
+            blue = x / width
+        elif self.blue_mode == 1:
+            blue = y / height
+        elif self.blue_mode == 2:
+            blue = 1 - (x / width)
+        elif self.blue_mode == 3:
+            blue = 1 - (y / height)
+        else:
+            blue = 0
+
+        return np.array((red, green, blue))
+
+    def handleOtherInput(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+
+                # Wraps around thanks to modulus logic
+                self.red_mode = (self.red_mode + 1) % 4
+                type(self).restart()
+            elif event.key == pygame.K_2:
+                self.green_mode = (self.green_mode + 1) % 4
+                type(self).restart()
+            elif event.key == pygame.K_3:
+                self.blue_mode = (self.blue_mode + 1) % 4
+                type(self).restart()
+
+# TODO: Create a NoiseRenderer class here
+
+
+class NoiseRenderer(ProgressiveRenderer):
+
+    # Shows how renderer is restarted
+    # This is mainly for artifacts that don't look right in lower res
+    def __init__(self, width=640, height=480,
+                 showTime=True,
+                 show=ShowTypes.PerColumn,
+                 minimumPixel=0,
+                 startPixelSize=256):
+        """An unnecessary override but provided to show how
+        to override the __init__ in future inheritance classes."""
+        super().__init__(width, height,
+                         showTime,
+                         show,
+                         minimumPixel,
+                         startPixelSize)
+
+      # Instance Properties --------------------------------
+
+      # An integer representing the current noise pattern id
+        self.pattern_id = 0
+
+      # Lambda documentation cited from https://realpython.com/python-lambda/
+
+        # self.patterns = [lambda x, y: NoisePatterns.getInstance().clouds(x, y)]
+        # self.patterns = [lambda x, y: NoisePatterns.getInstance().cloudsTiled(x, y,5,5)]
+        # self.patterns = [lambda x, y: NoisePatterns.getInstance().marble(x, y)]
+        # self.patterns = [lambda x, y: NoisePatterns.getInstance().wood(x, y)]
+        # self.patterns = [lambda x, y: NoisePatterns.getInstance().fire(x, y)]
+        
+        self.patterns = [
+            lambda x, y: NoisePatterns.getInstance().clouds(x, y),
+            lambda x, y: NoisePatterns.getInstance().cloudsTiled(x, y, 3, 3),
+            lambda x, y: NoisePatterns.getInstance().marble(x, y, noiseStrength=0),
+            lambda x, y: NoisePatterns.getInstance().wood(x, y, noiseStrength=0 ),
+            lambda x, y: NoisePatterns.getInstance().fire(x, y,noiseStrength=0)
+        ]
+
+
+      # Instance Methods ----------------------------------------------------------------
+
+      # and a list of NoisePatterns method references.
+
+    def getColor(self, x, y, scale=64):
+        """Gives a random color per pixel."""
+        # divides x and y by the scale and then calls the current method of noise.
+        # While the methods in NoisePatterns return colors in 1.0 mode,
+        # the progressive renderer will multiply by 255 to satisfy PyGame.
+
+        scaled_x = x/scale
+        scaled_y = y/scale
+
+        return self.patterns[self.pattern_id](scaled_x, scaled_y)
+
+    def handleOtherInput(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                # Wraps around thanks to modulus logic
+                self.pattern_id = (self.pattern_id-1) % len(self.patterns)
+                type(self).restart()
+            elif event.key == pygame.K_w:
+                self.pattern_id = (self.pattern_id+1) % len(self.patterns)
+                type(self).restart()
+            elif event.key == pygame.K_e:
+                NoisePatterns.getInstance().previous()
+                type(self).restart()
+            elif event.key == pygame.K_r:
+                NoisePatterns.getInstance().next()
+                type(self).restart()
+
+
 # Calls the 'main' function when this script is executed
 if __name__ == '__main__':
     try:
-        RandomRenderer.main()
+        # RandomRenderer.main()
+        RainbowRenderer.main()
+        #NoiseRenderer.main() 
     finally:
         pygame.quit()
